54cb401fcbe88b2f846bac26b4b1a51bb11844fe
YASHASWI <ygoh0034@student.monash.edu>
2025-08-08 22:32:10 +1000

    tasks performed in week 2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import JSON from './components/JSON.vue'
 
-createApp(App).mount('#app')
+createApp(JSON).mount('#app')
